d11ba63d3a4f3366106936009bb2b3571b8a9e07
WoodenEasy <2314998529@qq.com>
2026-04-16 12:41:39 +0800

    refactor:修改setting配置，改变字符串导入urls配置为python模块导入

--- src/pocket_api/config/settings.py
+++ src/pocket_api/config/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from pocket_api.config import urls
+
 port = os.environ.get("DJANGO_PORT", 8080)
 try:
     port = int(port)
@@ -7,6 +9,6 @@ except TypeError:
     raise TypeError(f"后端端口不是整数，端口为{port}")
 
 DEBUG = True
-ROOT_URLCONF = "pocket_api.config.urls"
+ROOT_URLCONF = urls.__name__
 PORT = port
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "*")
